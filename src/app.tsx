3dd344650ac1aa77bfaf1d237bf34e7f730d0411
VoskanianGor <vosckaniangor@gmail.com>
2022-12-29 12:02:33 +0300

    chore: added sass package

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import Button from '~components/button'
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button />
     </div>
   )
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
+import styles from './styles.module.scss'
 
 export default function Button() {
-  return <button type='button'>press</button>
+  return <button className={styles.button} type='button'>press</button>
 }
