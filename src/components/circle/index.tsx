d5735541cd3535f9113ac8e920a137285582f902
VoskanianGor <vosckaniangor@gmail.com>
2022-12-29 21:12:34 +0300

    feat: page layout lines

--- src/components/circle/index.tsx
+++ src/components/circle/index.tsx
@@ -2,6 +2,7 @@ import React, { useLayoutEffect, useRef, useState } from 'react'
 import type { FC } from 'react'
 import useCirclePoints from '~hooks/use-circle-points'
 import CircleOutline from './circle-outline'
+import Dates from '~components/dates'
 import Point from './point'
 import styles from './styles.module.scss'
 
@@ -14,24 +15,27 @@ const Circle: FC<ICircle> = ({ timePeriods }) => {
     useCirclePoints(timePeriods)
 
   return (
-    <div
-      className={styles.circle}
-      ref={circleRef}
-      style={{
-        transform: `rotate(${rotate}deg)`,
-      }}
-    >
-      <CircleOutline />
-      {points?.map((cords, index) => (
-        <Point
-          key={index}
-          index={index}
-          cords={cords}
-          isActive={index === activePoint}
-          circleRotate={rotate}
-          onPointClick={onPointClick}
-        />
-      ))}
+    <div className={styles.wrapper}>
+      <Dates />
+      <div
+        className={styles.circle}
+        ref={circleRef}
+        style={{
+          transform: `rotate(${rotate}deg)`,
+        }}
+      >
+        <CircleOutline />
+        {points?.map((cords, index) => (
+          <Point
+            key={index}
+            index={index}
+            cords={cords}
+            isActive={index === activePoint}
+            circleRotate={rotate}
+            onPointClick={onPointClick}
+          />
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/components/dates/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+const Dates = () => {
+  return (
+    <div className={styles.dates}>
+      <div className={styles.start}>2015</div>
+      <div className={styles.end}>2022</div>
+    </div>
+  )
+}
+
+export default Dates
