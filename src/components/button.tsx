58cd92124ee5b7888a769c01d8d3c007b89746da
VoskanianGor <vosckaniangor@gmail.com>
2022-12-29 11:54:12 +0300

    chore: added rome

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
 
 export default function Button() {
-	return <button>press</button>
+  return <button type='button'>press</button>
 }
--- src/typings/images.d.ts
+++ src/typings/images.d.ts
@@ -1,30 +1,30 @@
 declare module '*.png' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
 
 declare module '*.jpg' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
 
 declare module '*.jpeg' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
 
 declare module '*.webp' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
 
 declare module '*.svg' {
-  import type * as React from 'react';
+  import type * as React from 'react'
 
   export const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
+  >
 
-  const src: any;
-  export default src;
+  const src: any
+  export default src
 }
