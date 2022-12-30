8de8a91851e55ffaf746b4d1c69fc8c71c984aa1
VoskanianGor <vosckaniangor@gmail.com>
2022-12-30 06:54:37 +0300

    feat: added animated years

--- src/hooks/use-global-state.ts
+++ src/hooks/use-global-state.ts
@@ -2,7 +2,6 @@ import { createGlobalState } from 'react-hooks-global-state'
 
 const { getGlobalState, setGlobalState, useGlobalState } = createGlobalState({
   activeTimePeriod: 0,
-  lastYear: 0,
 })
 
 export { getGlobalState, setGlobalState, useGlobalState }
